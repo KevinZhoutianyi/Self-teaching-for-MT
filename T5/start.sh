c9f2e7ce1320df4b4662c43be969256b87a27f1f
kevin zhou <kevinzhou2@live.cn>
2022-03-04 14:44:06 -0800

    start.sh

--- T5/start.sh
+++ T5/start.sh
@@ -1,2 +1,5 @@
-conda activate tianyi
-python3 ./main.py
+cd /tianyi-vol/Self-teaching-for-machine-translation/T5
+pip install transformers
+pip install datasets
+pip install sacrebleu==1.5.1
+mkdir log
