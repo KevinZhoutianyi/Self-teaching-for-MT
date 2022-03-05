37b9f6734330ed7f118910223f72344bc7b63a21
kevin zhou <kevinzhou2@live.cn>
2022-03-05 13:29:04 -0800

    .

--- T5/start.sh
+++ T5/start.sh
@@ -3,5 +3,7 @@ pip install transformers
 pip install datasets
 pip install sacrebleu==1.5.1
 mkdir log
+mkdir model
+mkdir tensorboard
 python main.py --valid_num_points 2000 --train_num_points 30000 --valid_begin 1
 
