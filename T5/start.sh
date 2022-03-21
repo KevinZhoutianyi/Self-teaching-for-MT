01c44285fb9cb7dee76e8c243f880f3b2d0268a1
kevin zhou <kevinzhou2@live.cn>
2022-03-21 16:40:13 -0700

    .

--- T5/start.sh
+++ T5/start.sh
@@ -7,5 +7,5 @@ mkdir model
 mkdir tensorboard
 rm -f ./log/*.txt
 rm -f ./tensorboard/*
-python main.py --valid_num_points 2000 --train_num_points 16000 --valid_begin 1 --train_A 1
+python main.py --valid_num_points 2000 --train_num_points 16000 --valid_begin 1 --train_A 0
 
