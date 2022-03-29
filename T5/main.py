f1ade932dbf57431fcea83612653b7e9bf950a88
kevin zhou <kevinzhou2@live.cn>
2022-03-29 12:11:56 -0700

    larger grad acc

--- T5/main.py
+++ T5/main.py
@@ -32,10 +32,10 @@ parser.add_argument('--valid_num_points', type=int,             default = 1000, 
 parser.add_argument('--train_num_points', type=int,             default = 4000, help='train data number')
 
 parser.add_argument('--batch_size', type=int,                   default=32,     help='Batch size')
-parser.add_argument('--train_w_num_points', type=int,           default=12,      help='train_w_num_points for each batch')
-parser.add_argument('--train_w_synthetic_num_points', type=int, default=12,      help='train_w_synthetic_num_points for each batch')
-parser.add_argument('--train_v_num_points', type=int,           default=4,      help='train_v_num_points for each batch')
-parser.add_argument('--train_A_num_points', type=int,           default=4,      help='train_A_num_points decay for each batch')
+parser.add_argument('--train_w_num_points', type=int,           default=8,      help='train_w_num_points for each batch')
+parser.add_argument('--train_w_synthetic_num_points', type=int, default=8,      help='train_w_synthetic_num_points for each batch')
+parser.add_argument('--train_v_num_points', type=int,           default=8,      help='train_v_num_points for each batch')
+parser.add_argument('--train_A_num_points', type=int,           default=8,      help='train_A_num_points decay for each batch')
 
 
 parser.add_argument('--gpu', type=int,                          default=0,      help='gpu device id')
@@ -45,9 +45,9 @@ parser.add_argument('--exp_name', type=str,                     default='t5base 
 parser.add_argument('--epochs', type=int,                       default=50,     help='num of training epochs')
 parser.add_argument('--pre_epochs', type=int,                   default=3,      help='train model W for x epoch first')
 parser.add_argument('--grad_clip', type=float,                  default=5,      help='gradient clipping')
-parser.add_argument('--grad_acc_count', type=float,             default=4,      help='gradient accumulate steps')
+parser.add_argument('--grad_acc_count', type=float,             default=8,      help='gradient accumulate steps')
 
-parser.add_argument('--w_lr', type=float,                       default=3e-5,   help='learning rate for w')
+parser.add_argument('--w_lr', type=float,                       default=2e-5,   help='learning rate for w')
 parser.add_argument('--v_lr', type=float,                       default=3e-4,   help='learning rate for v')
 parser.add_argument('--A_lr', type=float,                       default=1e-4,   help='learning rate for A')
 parser.add_argument('--learning_rate_min', type=float,          default=1e-8,   help='learning_rate_min')
