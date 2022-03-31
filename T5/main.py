994ef0285e3f727c3efda63ff165cc498ea1bf14
kevin zhou <kevinzhou2@live.cn>
2022-03-31 01:13:57 -0700

    .

--- T5/main.py
+++ T5/main.py
@@ -39,7 +39,7 @@ parser.add_argument('--train_A_num_points', type=int,           default=4,      
 
 parser.add_argument('--gpu', type=int,                          default=0,      help='gpu device id')
 parser.add_argument('--model_name', type=str,                   default='t5-small',      help='gpu device id')
-parser.add_argument('--exp_name', type=str,                     default='clip',      help='gpu device id')
+parser.add_argument('--exp_name', type=str,                     default='no V 10 clip',      help='gpu device id')
 
 parser.add_argument('--epochs', type=int,                       default=50,     help='num of training epochs')
 parser.add_argument('--pre_epochs', type=int,                   default=0,      help='train model W for x epoch first')
