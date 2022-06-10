71d9a915abbe1ad649edb719a431c772fd558138
kevin zhou <kevinzhou2@live.cn>
2022-06-10 00:38:18 -0700

    .

--- T5/start.sh
+++ T5/start.sh
@@ -10,9 +10,9 @@ mkdir tensorboard
 rm -f ./log/*.txt
 rm -f ./tensorboard/*
 python main.py --valid_num_points 2000 --train_num_points 50000 \
-                --batch_size 128 --rep_num 1000 --test_num 10000\
-                --train_w_num_points 32 --train_v_synthetic_num_points 32\
-                --train_v_num_points 32 --train_A_num_points 32\
+                --batch_size 256 --rep_num 1000 --test_num 10000\
+                --train_w_num_points 64 --train_v_synthetic_num_points 64\
+                --train_v_num_points 64 --train_A_num_points 64\
                 --valid_begin 1 --train_A 1  --model_name_student t5-small --model_name_teacher t5-small\
                 --w_lr 5e-4 --v_lr 5e-4 --A_lr 1e-3\
                 --exp_name server,50k
