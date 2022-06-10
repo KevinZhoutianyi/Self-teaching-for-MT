dcad143346c6c4835b65ac632d6d38e1befd0ec0
kevin zhou <kevinzhou2@live.cn>
2022-06-09 22:31:40 -0700

    .

--- T5/start.sh
+++ T5/start.sh
@@ -13,7 +13,7 @@ python main.py --valid_num_points 2000 --train_num_points 50000 \
                 --batch_size 512 --rep_num 1000 --test_num 10000\
                 --train_w_num_points 128 --train_v_synthetic_num_points 128\
                 --train_v_num_points 128 --train_A_num_points 128\
-                --valid_begin 0 --train_A 0  --model_name t5-small\
+                --valid_begin 0 --train_A 0  --model_name_student t5-small --model_name_teacher t5-base\
                 --w_lr 0 --v_lr 5e-4 --A_lr 1e-3\
                 --exp_name server,50k
 
