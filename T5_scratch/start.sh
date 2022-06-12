4c6520edbee709b641947338beee96f684711f8f
kevin zhou <kevinzhou2@live.cn>
2022-06-12 16:15:45 -0700

    .

--- T5_scratch/start.sh
+++ T5_scratch/start.sh
@@ -1,4 +1,4 @@
-cd /tianyi-vol/Self-teaching-for-machine-translation/T5
+cd /tianyi-vol/Self-teaching-for-machine-translation/T5_scratch
 pip install transformers
 pip install datasets
 pip install sacrebleu==1.5.1
@@ -14,6 +14,6 @@ python main.py --valid_num_points 2000 --train_num_points 50000 \
                 --train_w_num_points 90 --train_v_synthetic_num_points 90\
                 --train_v_num_points 90 --train_A_num_points 90\
                 --valid_begin 1 --train_A 1  --model_name_student t5-small --model_name_teacher t5-small\
-                --w_lr 5e-4 --v_lr 5e-4 --A_lr 1 --unrolled_w_lr 5e-4 --unrolled_v_lr 5e-4 \
-                --exp_name server,50k,withA
+                --w_lr 1 --v_lr 0 --A_lr 01 --unrolled_w_lr 0 --unrolled_v_lr 0 \
+                --exp_name server,50k,scratch
 
