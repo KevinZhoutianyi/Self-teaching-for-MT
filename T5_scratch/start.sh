a7dd92bc1e38007473017529066dc1d72ddba7b4
kevin zhou <kevinzhou2@live.cn>
2022-06-13 00:17:17 -0700

    .

--- T5_scratch/start.sh
+++ T5_scratch/start.sh
@@ -10,8 +10,8 @@ mkdir tensorboard
 rm -f ./log/*.txt
 rm -f ./tensorboard/*
 python main.py --valid_num_points 3000 --train_num_points 3000000 \
-                --batch_size 720 --rep_num 2000 --test_num 1000000\
-                --train_w_num_points 720 --train_v_synthetic_num_points 0\
+                --batch_size 400 --rep_num 2000 --test_num 1000000\
+                --train_w_num_points 400 --train_v_synthetic_num_points 0\
                 --train_v_num_points 0 --train_A_num_points 0\
                 --valid_begin 1 --train_A 1  --model_name_student google/t5-small-lm-adapt --model_name_teacher t5-small\
                 --w_lr 1e-3 --v_lr 0 --A_lr 01 --unrolled_w_lr 0 --unrolled_v_lr 0 \
