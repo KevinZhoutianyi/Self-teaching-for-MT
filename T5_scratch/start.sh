4db355f6fb7209c2f0933a9023f37218c00f230d
kevin zhou <kevinzhou2@live.cn>
2022-06-15 00:59:28 -0700

    .

--- T5_scratch/start.sh
+++ T5_scratch/start.sh
@@ -15,6 +15,6 @@ python main.py --valid_num_points 3000 --train_num_points 3000000 \
                 --train_w_num_points 64 --train_v_synthetic_num_points 128\
                 --train_v_num_points 0 --train_A_num_points 64\
                 --valid_begin 1 --train_A 1  --model_name_teacher google/t5-small-lm-adapt --model_name_student google/t5-small-lm-adapt\
-                --w_lr 1e-3 --v_lr 1e-3 --A_lr 0.1 --unrolled_w_lr 1e-3 --unrolled_v_lr 1e-3 \
-                --exp_name server,3kk,withA,0.1
+                --w_lr 1e-3 --v_lr 1e-3 --A_lr 1 --unrolled_w_lr 1e-3 --unrolled_v_lr 1e-3 \
+                --exp_name server,3kk,withA,1
 
