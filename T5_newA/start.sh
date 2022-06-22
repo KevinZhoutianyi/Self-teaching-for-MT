2b4f46d1dc86eb3f98c1e05c7c3e9a22761970bf
kevin zhou <kevinzhou2@live.cn>
2022-06-21 22:18:35 -0700

    .

--- T5_newA/start.sh
+++ T5_newA/start.sh
@@ -16,5 +16,5 @@ python main.py --valid_num_points 3000 --train_num_points 1000000 \
                 --train_v_num_points 0 --train_A_num_points 78 --pre_epochs 1\
                 --valid_begin 0 --train_A 1  --model_name_teacher google/t5-small-lm-adapt --model_name_student google/t5-small-lm-adapt --model_name_de2en Onlydrinkwater/t5-small-de-en-mt\
                 --w_lr 1e-3 --v_lr 1e-3 --A_lr 1e-3  --unrolled_w_lr 1e-3 --unrolled_v_lr 1e-3 --beta1 0.9 --beta2 0.98  --freeze 0\
-                --exp_name BETA11,eval,noisedata,nofreeze
+                --exp_name BETA11,eval,noisedata,nofreeze,pre1
 
