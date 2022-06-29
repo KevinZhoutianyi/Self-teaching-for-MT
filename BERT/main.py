7ceec14dadddb52eebdaa78d4e4cb490524ae951
kevin zhou <kevinzhou2@live.cn>
2022-06-28 22:39:25 -0700

    .

--- BERT/main.py
+++ BERT/main.py
@@ -48,7 +48,7 @@ parser.add_argument('--num_workers', type=int,                  default=0,      
 # parser.add_argument('--model_name_teacher', type=str,           default='prajjwal1/bert-small',      help='model_name')
 # parser.add_argument('--model_name_student', type=str,           default='prajjwal1/bert-small',      help='model_name')
 # parser.add_argument('--model_name_de2en', type=str,             default='prajjwal1/bert-small',      help='model_name')
-parser.add_argument('--exp_name', type=str,                     default='T5spec',      help='experiment name')
+parser.add_argument('--exp_name', type=str,                     default='SST2',      help='experiment name')
 parser.add_argument('--rep_num', type=int,                      default=2500,      help='report times for 1 epoch')
 parser.add_argument('--test_num', type=int,                     default=30000,      help='test times for 1 epoch')
 
