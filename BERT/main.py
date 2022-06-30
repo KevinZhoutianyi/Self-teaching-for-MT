cbef8708ab4ac20cf4b4e2f9478c7f407d2382c6
kevin zhou <kevinzhou2@live.cn>
2022-06-30 00:06:30 -0700

    .

--- BERT/main.py
+++ BERT/main.py
@@ -77,7 +77,7 @@ parser.add_argument('--traindata_loss_ratio', type=float,       default=0.7,    
 parser.add_argument('--syndata_loss_ratio', type=float,         default=0.3,    help='augmented dataset ratio')
 
 parser.add_argument('--valid_begin', type=int,                  default=1,      help='whether valid before train')
-parser.add_argument('--train_A', type=int,                      default=0 ,     help='whether train A')
+parser.add_argument('--train_A', type=int,                      default=1 ,     help='whether train A')
 
 parser.add_argument('--embedding_dim', type=int,                default=300 ,     help='whether train A')
 parser.add_argument('--out_dim', type=int,                      default=2 ,     help='whether train A')
@@ -416,9 +416,9 @@ def my_train(epoch, _dataloader, validdataloader, w_model, v_model, architect, A
 
 
 # %%
-if(args.valid_begin == 1):
-    my_test(valid_dataloader, model_w, -1)  # before train
-    my_test(valid_dataloader, model_v, -1)
+# if(args.valid_begin == 1):
+#     my_test(valid_dataloader, model_w, -1)  # before train
+#     my_test(valid_dataloader, model_v, -1)
 
 tot_iter = [0]
 v_accu = 0
