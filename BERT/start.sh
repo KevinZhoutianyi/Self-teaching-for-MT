60dc8a66245c02f393bc32d7a3ace3e7f8a29840
kevin zhou <kevinzhou2@live.cn>
2022-06-30 11:09:53 -0700

    .

--- BERT/start.sh
+++ BERT/start.sh
@@ -1,3 +1,3 @@
-python main.py --A_lr 2e-5 --batch_size 48 --train_w_num_points 16 --train_v_synthetic_num_points 16 --train_A_num_points 16 \
+python main.py --A_lr 2e-4 --batch_size 48 --train_w_num_points 16 --train_v_synthetic_num_points 16 --train_A_num_points 16 \
 --train_num_points 30000 --rep_num 5000 --test_num 30000 \
---exp_name SST2,e5
+--exp_name SST2,e4
