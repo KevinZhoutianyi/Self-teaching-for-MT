13a80d1ee024b31acad9333509ef82ef3d135551
kevin zhou <kevinzhou2@live.cn>
2022-07-01 19:38:59 -0700

    .

--- BERT/start.sh
+++ BERT/start.sh
@@ -1,3 +1,3 @@
 python main.py --A_lr 2e-6 --batch_size 48 --train_w_num_points 16 --train_v_synthetic_num_points 16 --train_A_num_points 16 \
---train_num_points 30000 --rep_num 5000 --test_num 30000 --traindata_loss_ratio 0 --syndata_loss_ratio 1\
+--train_num_points 30000 --rep_num 5000 --test_num 30000 --traindata_loss_ratio 0 --syndata_loss_ratio 1 \
 --exp_name SST2,halfattack,nonorm,onlySyn
