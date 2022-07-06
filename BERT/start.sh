b6059445a1d24681af558eb3a6138fee6ed518f0
kevin zhou <kevinzhou2@live.cn>
2022-07-06 13:17:39 -0700

    .

--- BERT/start.sh
+++ BERT/start.sh
@@ -1,1 +1,1 @@
-python main.py --train_A 1 --A_lr 5 --A_bs 16 --epochs 30 --exp_name Yelp,savedreal,diffAlr
+python main.py --train_A 1 --A_lr 50 --A_bs 16 --epochs 30 --exp_name Yelp,savedreal,diffAlr
