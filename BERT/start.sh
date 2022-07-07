5112e56e350da355fa13919c598feb644aa9c056
kevin zhou <kevinzhou2@live.cn>
2022-07-07 04:46:19 +0000

    .

--- BERT/start.sh
+++ BERT/start.sh
@@ -1,2 +1,2 @@
-python main.py --train_A 0 --A_lr 100 --A_bs 16 --epochs 30 --clean_A_data 1 --train_w_num_points 4000 --exp_name Yelp,savedreal,diffAlr \
- --A_path /tianyi-vol/Self-teaching-for-machine-translation/BERT/trainedA/A.pt
+python main.py --train_A 1 --A_lr 100 --A_bs 16 --epochs 30 --clean_A_data 1 --train_w_num_points 4000 --exp_name Yelp,savedreal,diffAlr \
+ --load_A 0 --A_path /tianyi-vol/Self-teaching-for-machine-translation/BERT/trainedA/A.pt
